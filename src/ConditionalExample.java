7f2ff7d2bae7d7e530502a5950b7a390770113c7
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2023-01-04 18:52:27 -0600

    Add files via upload

---
+++ src/ConditionalExample.java
@@ -0,0 +1,54 @@
+
+public class ConditionalExample {
+
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		
+		System.out.println("a = "+a);
+		System.out.println("b = "+b);
+		System.out.println("c = "+c);
+		
+		if (a < b) {
+			System.out.println("a < b");
+		}
+		if (a > b) {
+			System.out.println("a > b");
+		}
+		if (a == b) {
+			System.out.println("a = b");
+		}
+		
+		if (a < b) {
+			System.out.println("a < b");
+		}
+		else if (a > b) { 
+			System.out.println("a > b");
+		}
+		else {
+			System.out.println("a = b");
+		}
+		
+		if (a < b && b < c && a < c) {
+			System.out.println("Numbers are in order");
+		}
+		else {
+			System.out.println("Numbers are not in order");
+		}
+		
+		switch (a) {
+			case 1 :
+				System.out.println("a = 1");
+				break;
+			case 2 :
+				System.out.println("a = 2");
+				break;
+			default:
+				System.out.println("Don't know what a is");
+		}
+
+	}
+
+}
