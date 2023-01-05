64284074609e74b01506c2603abe19a7d7f1f983
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2023-01-04 18:54:25 -0600

    Add files via upload

---
+++ src/CrazyRandom.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+public class CrazyRandom {
+
+	public static void main(String[] args) {
+		
+		Random randomgen;
+		randomgen = new Random();
+	//	randomgen.setSeed(123456);
+		int total = 0;
+		int roll;
+		int sides = 6;
+		
+		for(int i = 0; i < 10; i++) {
+		  roll = randomgen.nextInt(sides)+1;
+		  System.out.println(roll);
+		  total = total + roll;
+		}
+		
+		System.out.println(total);
+	}
+
+}
