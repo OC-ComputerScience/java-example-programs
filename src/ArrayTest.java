3716b3c7a738a7a4a5ea4295b4a1c6b3f6e90b37
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2023-01-11 20:47:17 -0600

    Add files via upload

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -1,36 +1,36 @@
- 
+
 public class ArrayTest {
 
 	public static void main(String[] args) {
-		
-		int[] numList = {1,2,3,5,8,9,10};
-		
+
+		int[] numList = { 1, 2, 3, 5, 8, 9, 10 };
+
 		for (int i = 0; i < numList.length; i++) {
 			System.out.println(i + ": " + numList[i]);
 		}
-		
-		String[] stringList = {"apple", "pear", "peach", "banana"};
-		
+
+		String[] stringList = { "apple", "pear", "peach", "banana" };
+
 		for (String s : stringList) {
 			System.out.println(s);
 		}
-		
-		int[][] numList2 = {{1,1},{2,2}};
-		
-		for (int x = 0; x < 2; x++) { 
-			
-			for (int y = 0; y < 2; y++ ) {
-				System.out.println("x = "+x+" y = "+y+": "+numList2[x][y]);
+
+		int[][] numList2 = { { 1, 1 }, { 2, 2 } };
+
+		for (int x = 0; x < 2; x++) {
+
+			for (int y = 0; y < 2; y++) {
+				System.out.println("x = " + x + " y = " + y + ": " + numList2[x][y]);
 			}
-			
+
 		}
-			
+
 		for (int[] i : numList2) {
-			for(int j : i) {
+			for (int j : i) {
 				System.out.println(j);
 			}
 		}
-		
+
 	}
 
 }
