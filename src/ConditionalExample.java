f3d8dce2c9fe447c5258fdb5335603a990a5af98
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2023-01-11 20:45:31 -0600

    Add files via upload

--- src/ConditionalExample.java
+++ src/ConditionalExample.java
@@ -2,15 +2,15 @@
 public class ConditionalExample {
 
 	public static void main(String[] args) {
-		
+
 		int a = 1;
 		int b = 2;
 		int c = 3;
-		
-		System.out.println("a = "+a);
-		System.out.println("b = "+b);
-		System.out.println("c = "+c);
-		
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+
 		if (a < b) {
 			System.out.println("a < b");
 		}
@@ -20,33 +20,30 @@ public class ConditionalExample {
 		if (a == b) {
 			System.out.println("a = b");
 		}
-		
+
 		if (a < b) {
 			System.out.println("a < b");
-		}
-		else if (a > b) { 
+		} else if (a > b) {
 			System.out.println("a > b");
-		}
-		else {
+		} else {
 			System.out.println("a = b");
 		}
-		
+
 		if (a < b && b < c && a < c) {
 			System.out.println("Numbers are in order");
-		}
-		else {
+		} else {
 			System.out.println("Numbers are not in order");
 		}
-		
+
 		switch (a) {
-			case 1 :
-				System.out.println("a = 1");
-				break;
-			case 2 :
-				System.out.println("a = 2");
-				break;
-			default:
-				System.out.println("Don't know what a is");
+		case 1:
+			System.out.println("a = 1");
+			break;
+		case 2:
+			System.out.println("a = 2");
+			break;
+		default:
+			System.out.println("Don't know what a is");
 		}
 
 	}
