01b2796f02316b5ee2162850ca86d374d92c4fb8
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2023-01-11 20:46:01 -0600

    Add files via upload

--- src/LoopExample.java
+++ src/LoopExample.java
@@ -2,34 +2,33 @@
 public class LoopExample {
 
 	public static void main(String[] args) {
-		
+
 		int i;
-		
-		//While loop
+
+		// While loop
 		System.out.println("While Loop");
 		i = 0;
-		
-		while (i < 10) { 
-		    System.out.println("Value: "+i);
-		    i++;
-		}
-	
-		//Do While
-		System.out.println("\nDo While Loop");
-		i = 0;
-		
-		do { 
-			System.out.println("Value: "+i);
+
+		while (i < 10) {
+			System.out.println("Value: " + i);
 			i++;
 		}
-		while (i < 10);
-	
-		//For Loop
+
+		// Do While
+		System.out.println("\nDo While Loop");
+		i = 0;
+
+		do {
+			System.out.println("Value: " + i);
+			i++;
+		} while (i < 10);
+
+		// For Loop
 		System.out.println("\nFor Loop");
-		
-		for (i = 0; i < 10; i++){ 
-			System.out.println("Value: "+i);
-		}	
+
+		for (i = 0; i < 10; i++) {
+			System.out.println("Value: " + i);
+		}
 
 	}
 
