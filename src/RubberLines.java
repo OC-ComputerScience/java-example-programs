7c9e1fea895d50fa0512bd910fed7c690078acdc
Chloe Sheasby <chloe.sheasby@eagles.oc.edu>
2023-01-11 20:48:12 -0600

    Add files via upload

--- src/RubberLines.java
+++ src/RubberLines.java
@@ -6,20 +6,15 @@
 
 import javax.swing.JFrame;
 
-public class RubberLines
-{
-   //-----------------------------------------------------------------
-   //  Creates and displays the application frame.
-   //-----------------------------------------------------------------
-   public static void main(String[] args)
-   {
-      JFrame frame = new JFrame("Rubber Lines");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.getContentPane().add(new RubberLinesPanel());
-      frame.pack();
-      frame.setVisible(true);
-   }
+public class RubberLines {
+	// -----------------------------------------------------------------
+	// Creates and displays the application frame.
+	// -----------------------------------------------------------------
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Rubber Lines");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(new RubberLinesPanel());
+		frame.pack();
+		frame.setVisible(true);
+	}
 }
-		
-
-
--- src/RubberLinesPanel.java
+++ src/RubberLinesPanel.java
@@ -4,74 +4,90 @@
 //  Represents the primary drawing panel for the RubberLines program.
 //********************************************************************
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.JPanel;
-import java.awt.*;
-import java.awt.event.*;
 
 @SuppressWarnings("serial")
-public class RubberLinesPanel extends JPanel
-{
-   private Point point1 = null, point2 = null;
+public class RubberLinesPanel extends JPanel {
+	private Point point1 = null, point2 = null;
 
-   //-----------------------------------------------------------------
-   //  Constructor: Sets up this panel to listen for mouse events.
-   //-----------------------------------------------------------------
-   public RubberLinesPanel()
-   {
-      LineListener listener = new LineListener();
-      addMouseListener(listener);
-      addMouseMotionListener(listener);
+	// -----------------------------------------------------------------
+	// Constructor: Sets up this panel to listen for mouse events.
+	// -----------------------------------------------------------------
+	public RubberLinesPanel() {
+		LineListener listener = new LineListener();
+		addMouseListener(listener);
+		addMouseMotionListener(listener);
 
-      setBackground(Color.black);
-      setPreferredSize(new Dimension(400, 200));
-   }		
-   //-----------------------------------------------------------------
-   //  Draws the current line from the initial mouse-pressed point to
-   //  the current position of the mouse.
-   //-----------------------------------------------------------------
-   public void paintComponent(Graphics page)
-   {
-      super.paintComponent(page);
+		setBackground(Color.black);
+		setPreferredSize(new Dimension(400, 200));
+	}
 
-      page.setColor (Color.yellow);
-      if (point1 != null && point2 != null) {
-    	 page.drawLine(point1.x, point1.y, point2.x, point2.y); 
-      }
-   }
+	// -----------------------------------------------------------------
+	// Draws the current line from the initial mouse-pressed point to
+	// the current position of the mouse.
+	// -----------------------------------------------------------------
+	@Override
+	public void paintComponent(Graphics page) {
+		super.paintComponent(page);
 
-   //*****************************************************************
-   //  Represents the listener for all mouse events.
-   //*****************************************************************
-   private class LineListener implements MouseListener,
-                                         MouseMotionListener
-   {
-      //--------------------------------------------------------------
-      //  Captures the initial position at which the mouse button is
-      //  pressed.
-      //--------------------------------------------------------------
-      public void mousePressed(MouseEvent event)
-      {
-         point1 = event.getPoint();
-      }		
+		page.setColor(Color.yellow);
+		if (point1 != null && point2 != null) {
+			page.drawLine(point1.x, point1.y, point2.x, point2.y);
+		}
+	}
 
-      //--------------------------------------------------------------
-      //  Gets the current position of the mouse as it is dragged and
-      //  redraws the line to create the rubberband effect.
-      //--------------------------------------------------------------
-      public void mouseDragged(MouseEvent event)
-      {
-         point2 = event.getPoint();
-         repaint();
-      }
+	// *****************************************************************
+	// Represents the listener for all mouse events.
+	// *****************************************************************
+	private class LineListener implements MouseListener, MouseMotionListener {
+		// --------------------------------------------------------------
+		// Captures the initial position at which the mouse button is
+		// pressed.
+		// --------------------------------------------------------------
+		@Override
+		public void mousePressed(MouseEvent event) {
+			point1 = event.getPoint();
+		}
 
-      //--------------------------------------------------------------
-      //  Provide empty definitions for unused event methods.
-      //--------------------------------------------------------------
-      public void mouseClicked(MouseEvent event) {}
-      public void mouseReleased(MouseEvent event) {}
-      public void mouseEntered(MouseEvent event) {}
-      public void mouseExited(MouseEvent event) {}
-      public void mouseMoved(MouseEvent event) {}
-   }
-}	
+		// --------------------------------------------------------------
+		// Gets the current position of the mouse as it is dragged and
+		// redraws the line to create the rubberband effect.
+		// --------------------------------------------------------------
+		@Override
+		public void mouseDragged(MouseEvent event) {
+			point2 = event.getPoint();
+			repaint();
+		}
 
+		// --------------------------------------------------------------
+		// Provide empty definitions for unused event methods.
+		// --------------------------------------------------------------
+		@Override
+		public void mouseClicked(MouseEvent event) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent event) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent event) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent event) {
+		}
+
+		@Override
+		public void mouseMoved(MouseEvent event) {
+		}
+	}
+}
